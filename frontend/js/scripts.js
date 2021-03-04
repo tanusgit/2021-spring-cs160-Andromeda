bbcbcd2a78a00a2fc101168b7293a7893832a648
Yulong Ran <yulong.ran@sjsu.edu>
2021-03-03 17:39:41 -0800

    Print api result on html with <p> tag

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -4,6 +4,9 @@ $.ajax({
   type: "json",
   success: function (response) {
     console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      $("body").append($("<div> Book title: " + response[i].title + "</div>"));
+    }
   },
   error: function (error) {
     console.log(error);
