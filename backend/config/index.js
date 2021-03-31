b44b76ede5a9df4db39535e1b287cdd231a67e4c
Yulong Ran <yulong.ran@sjsu.edu>
2021-03-30 21:22:18 -0700

    Added backedn configuration

---
+++ backend/config/index.js
@@ -0,0 +1,29 @@
+/**
+ * Server configurations
+ */
+const production = {
+  SERVER: "http://localhost",
+  SECERT: "faji4nme",
+  GOOGLE_CLIENT_ID: "46488740012-dkiq3b78ktmldmvvemqbuk6t0gai0ait.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "70ahRr-zi6Qisr92xPc1alGk",
+  DB_HOST: "localhost",
+  DB_USENAME: "cs160",
+  DB_PASSWORD: "password",
+  DB_DATABASE: "iBook",
+};
+
+const development = {
+  SERVER: "http://localhost",
+  SECERT: "faji4nme",
+  GOOGLE_CLIENT_ID: "46488740012-dkiq3b78ktmldmvvemqbuk6t0gai0ait.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "70ahRr-zi6Qisr92xPc1alGk",
+  DB_HOST: "localhost",
+  DB_USENAME: "cs160",
+  DB_PASSWORD: "password",
+  DB_DATABASE: "iBook",
+};
+
+module.exports = {
+  production,
+  development,
+};
