c9e713a2cba71c034730b47f57c9f18a4cec08c5
Yulong Ran <yulong.ran@sjsu.edu>
2021-03-31 16:12:47 -0700

    Added updateUser, getUser api

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,20 +1,30 @@
-const connection = require("../mysql");
-const getUser = require("../services/user").getUser;
+const { getUser, updateUser } = require("../services/user");
+
 /**
  * User api
  */
 
 const userAPI = (app) => {
   app.get("/api/user", (req, res) => {
-    getUser().then(
+    getUser(req.user.email).then(
       (result) => {
-        res.status(200).send(result);
+        return res.status(200).send(result);
       },
       (error) => {
-        res.status(404).send(error);
+        return res.status(error.code).send({ status: error.status });
       }
     );
-    return;
+  });
+
+  app.put("/api/user", (req, res) => {
+    updateUser(req.user.email, req.body.phone, req.body.address).then(
+      (result) => {
+        return res.status(200).send();
+      },
+      (error) => {
+        return res.status(error.code).send({ status: error.status });
+      }
+    );
   });
 };
 
--- backend/services/user.js
+++ backend/services/user.js
@@ -1,10 +1,51 @@
-const getUser = () => {
+const User = require("../models/user");
+
+/**
+ * Get user profile
+ * @param {String} email email of the user
+ * @returns User profile
+ */
+const getUser = (email) => {
   return new Promise((resolve, reject) => {
-    if (false) reject({ error: "Return error" });
-    resolve({ name: "Hello World" });
+    User.findOne({ where: { email: email }, raw: true })
+      .then(function (user) {
+        if (!user) {
+          reject({ status: "user not found", code: 404 });
+        }
+        resolve(user);
+      })
+      .catch(function (error) {
+        reject({ status: "server error", code: 500 });
+      });
+  });
+};
+
+/**
+ * Update user information
+ * @param {*} phone Phone number of the user
+ * @param {*} address Address of the user
+ * @returns Updated user object
+ */
+const updateUser = (email, phone, address) => {
+  return new Promise((resolve, reject) => {
+    User.update(
+      { phone: phone, address: address },
+      {
+        where: {
+          email: email,
+        },
+      }
+    )
+      .then((updatedUser) => {
+        resolve(updatedUser);
+      })
+      .catch((error) => {
+        reject({ status: "server error", code: 500 });
+      });
   });
 };
 
 module.exports = {
   getUser,
+  updateUser,
 };
