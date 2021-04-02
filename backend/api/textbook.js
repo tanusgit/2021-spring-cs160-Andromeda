7609c79de92e44bc012f19e62d72dfa7d825c0cd
William Tsai <william.tsai@sjsu.edu>
2021-04-01 21:30:10 -0700

    added get top books feature and crash prevention

--- backend/api/textbook.js
+++ backend/api/textbook.js
@@ -6,7 +6,7 @@ const connection = require("../mysql");
 const textbookAPI = (app) => {
   app.get("/api/textbook/all", (req, res) => {
     connection.query("SELECT * FROM book", function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(rows);
     });
   });
@@ -14,7 +14,15 @@ const textbookAPI = (app) => {
   //Retrieve a single book
   app.get("/api/textbook/:textbook_id", (req, res) => {
     connection.query(`SELECT * FROM book WHERE id = ${req.params.textbook_id}`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
+      res.status(200).send(rows);
+    });
+  });
+
+  //List the first few books based on size given
+  app.get("/api/textbook/popular/:size", (req, res) => {
+    connection.query(`SELECT * FROM book LIMIT ${req.params.size}`, function (err, rows, fields) {
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(rows);
     });
   });
@@ -22,7 +30,7 @@ const textbookAPI = (app) => {
   //List all book from specific author
   app.get("/api/textbook/author/:author_name", (req, res) => {
     connection.query(`SELECT * FROM book WHERE author_name = "${req.params.author_name}"`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(rows);
     });
   });
@@ -30,7 +38,7 @@ const textbookAPI = (app) => {
   //Create a book entity
   app.post("/api/textbook/create", (req, res) => {
     connection.query(`INSERT INTO book VALUES ("${req.body.id}", "${req.body.isbn}", "${req.body.publisher_name}", "${req.body.author_name}", "${req.body.year}", "${req.body.title}", "${req.body.price}", "${req.body.user_id}")`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(req.body);
     });
   });
@@ -38,7 +46,7 @@ const textbookAPI = (app) => {
   //Update a book information by id
   app.put("/api/textbook/update", (req, res) => {
     connection.query(`UPDATE book SET isbn="${req.body.isbn}", publisher_name="${req.body.publisher_name}", author_name="${req.body.author_name}", year="${req.body.year}", title="${req.body.title}", price="${req.body.price}", user_id="${req.body.user_id}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(req.body);
     });
   });
@@ -46,14 +54,14 @@ const textbookAPI = (app) => {
   //Delete a specific book
   app.delete("/api/textbook/delete/:textbook_id", (req, res) => {
     connection.query(`DELETE FROM book WHERE id = ${req.params.textbook_id}`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(rows);
     });
   });
 
   app.post("/api/textbook/test", (req, res) => {
     connection.query(`SELECT * FROM book`, function (err, rows, fields) {
-      if (err) throw err;
+      if (err) {return res.status(500).send("server error or input invalid");} 
       res.status(200).send(rows);
     });
   });
