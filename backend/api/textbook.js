b142248b239bac87f99525486420bb1086326690
William Tsai <william.tsai@sjsu.edu>
2021-04-22 17:42:55 -0700

    expanded update functions

--- backend/api/textbook.js
+++ backend/api/textbook.js
@@ -43,14 +43,164 @@ const textbookAPI = (app) => {
     });
   });
   
-  //Update a book information by id
-  app.put("/api/textbook/update", (req, res) => {
-    connection.query(`UPDATE textbook SET isbn="${req.body.isbn}", publisher_name="${req.body.publisher_name}", author_name="${req.body.author_name}", year="${req.body.year}", title="${req.body.title}", price="${req.body.price}", user_id="${req.body.user_id}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
-      if (err) {return res.status(500).send("server error or input invalid");} 
-      res.status(200).send(req.body);
-    });
+  //Update a textbook by id
+  app.put("/api/textbook/update", (req, res) => {      
+    try{
+        if(req.body.id === undefined) {
+          return res.status(500).send("update error: textbook id undefined");
+        }
+        parseInt(req.body.id, 10);
+        connection.query(`UPDATE textbook SET isbn="${req.body.isbn}", publisher_name="${req.body.publisher_name}", author_name="${req.body.author_name}", year="${req.body.year}", title="${req.body.title}", price="${req.body.price}", user_id="${req.body.user_id}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+          if (err) {return res.status(500).send("server error or input invalid");} 
+          res.status(200).send("textbook updated successfully");
+        });
+      }
+      catch {
+        res.status(500).send("server error or input invalid");
+      }
+  });
+
+
+  //Update a textbook ISBN by id
+  app.put("/api/textbook/updateISBN", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.isbn === undefined) {
+        return res.status(500).send("update error: textbook isbn undefined")
+      }
+      connection.query(`UPDATE textbook SET isbn="${req.body.isbn}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
   });
   
+  //Update a textbook title by id
+  app.put("/api/textbook/updateTitle", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.title === undefined) {
+        return res.status(500).send("update error: textbook title undefined")
+      }
+      connection.query(`UPDATE textbook SET title="${req.body.title}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
+  //Update a textbook author name by id
+  app.put("/api/textbook/updateAuthorName", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.author_name === undefined) {
+        return res.status(500).send("update error: textbook author name undefined")
+      }
+      connection.query(`UPDATE textbook SET author_name="${req.body.author_name}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
+  //Update a textbook year by id
+  app.put("/api/textbook/updateYear", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.year === undefined) {
+        return res.status(500).send("update error: textbook year undefined")
+      }
+      connection.query(`UPDATE textbook SET year="${req.body.year}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
+  //Update a textbook publisher name by id
+  app.put("/api/textbook/updatePublisherName", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.publisher_name === undefined) {
+        return res.status(500).send("update error: textbook publisher name undefined")
+      }
+      connection.query(`UPDATE textbook SET publisher_name="${req.body.publisher_name}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
+  //Update a textbook price by id
+  app.put("/api/textbook/updatePrice", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.price === undefined) {
+        return res.status(500).send("update error: textbook isbn undefined")
+      }
+      connection.query(`UPDATE textbook SET price="${req.body.price}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
+  //Update a textbook user id by id
+  app.put("/api/textbook/updateUserID", (req, res) => {
+    try{
+      if(req.body.id === undefined) {
+        return res.status(500).send("update error: textbook id undefined");
+      }
+      parseInt(req.body.id, 10);
+      if(req.body.user_id === undefined) {
+        return res.status(500).send("update error: textbook user id undefined")
+      }
+      connection.query(`UPDATE textbook SET user_id="${req.body.user_id}" WHERE id="${req.body.id}"`, function (err, rows, fields) {
+        if (err) {return res.status(500).send("server error or input invalid");} 
+        res.status(200).send("textbook updated successfully");
+      });
+    }
+    catch {
+      res.status(500).send("server error or input invalid");
+    }
+  });
+
   //Delete a specific book
   app.delete("/api/textbook/delete/:textbook_id", (req, res) => {
     connection.query(`DELETE FROM textbook WHERE id = ${req.params.textbook_id}`, function (err, rows, fields) {
